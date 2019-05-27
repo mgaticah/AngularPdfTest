e6279fe362766deecf56a78ed947c3f9a1225dac
Neoris <neoris@MANUELGATICA.local>
2019-05-27 12:05:12 -0400

    Inclusión de Input en componente para recibir parámetro src. Para probar, muestra el valor entregado por consola.

--- src/lib/pdf-renderer/pdf-renderer.component.ts
+++ src/lib/pdf-renderer/pdf-renderer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pdf-renderer',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pdf-renderer.component.scss']
 })
 export class PdfRendererComponent implements OnInit {
-
+  @Input() src: string;
   constructor() { }
 
   ngOnInit() {
+    console.log(this.src);
   }
+
 
 }
