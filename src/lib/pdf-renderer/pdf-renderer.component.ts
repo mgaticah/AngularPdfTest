94bfadea70f864acc93443d0f79409a28f7a26c6
Neoris <neoris@MANUELGATICA.local>
2019-05-28 14:54:38 -0400

    commit inicial con renderer client refiriendo a pdf-renderer

--- src/lib/pdf-renderer/pdf-renderer.component.ts
+++ src/lib/pdf-renderer/pdf-renderer.component.ts
@@ -14,34 +14,49 @@ export class PdfRendererComponent implements OnChanges {
   statusText: string;
   loadStatus: LoadStatus;
   constructor() { }
-  validatedSrc:any;
-  ngOnChanges(changes:SimpleChanges)
-  {
-    console.log(changes);
+  validatedSrc: any;
+  ngOnChanges(changes: SimpleChanges) {
+    for (let propName in changes) {
+      if (propName == 'pdfSrc') {
+        let change = changes[propName];
+        let curVal = JSON.stringify(change.currentValue);
+        let prevVal = JSON.stringify(change.previousValue);
+        if (curVal != prevVal && curVal != undefined) {
+          this.loadPdf(curVal);
+        }
+        else if(curVal==undefined)
+        {
+          this.loadedEvent.emit(false);
+          this.statusText='No se ha definido archivo';     
+        }
+      }
+    }
   }
-  loadPdf() {
-    // mock: inicia carga
+  loadPdf(pdfSrc) {
+    this.validatedSrc=pdfSrc;
+   
+   console.log(this.validatedSrc);
     this.loadedEvent.emit(false);
     // mock: cargando
     this.statusText = 'Cargando...';
-    this.loadStatus = LoadStatus.Loading;    
+    this.loadStatus = LoadStatus.Loading;
   }
   pdfLoaded(pdf: PDFDocumentProxy) {
     this.loadedEvent.emit(true);
     this.loadStatus = LoadStatus.Loaded;
     this.statusText = 'Cargado!';
 
- }
- pdfLoadingError(error: any) {
-  this.loadedEvent.emit(false);
-  this.loadStatus = LoadStatus.Error;
-  this.statusText = error.details;
-  console.error(error);
-}
-onProgress(progressData: PDFProgressData) {
-  // do anything with progress data. For example progress indicator
-console.log(`{{progressData.loaded}} of {{progressData.total}}`);
-}
+  }
+  pdfLoadingError(error: any) {
+    this.loadedEvent.emit(false);
+    this.loadStatus = LoadStatus.Error;
+    this.statusText = error.details;
+    console.error(error);
+  }
+  onProgress(progressData: PDFProgressData) {
+    // do anything with progress data. For example progress indicator
+    console.log(`{{progressData.loaded}} of {{progressData.total}}`);
+  }
 
 }
 
