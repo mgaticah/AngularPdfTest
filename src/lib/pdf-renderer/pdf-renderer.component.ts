92f310a500756f59de5cd7e5c3b2704f2caec92a
Neoris <neoris@MANUELGATICA.local>
2019-05-29 10:37:51 -0400

    Definiendo atributos para parametrizar el comportamiento del componente

--- src/lib/pdf-renderer/pdf-renderer.component.ts
+++ src/lib/pdf-renderer/pdf-renderer.component.ts
@@ -9,13 +9,20 @@ import { PDFDocumentProxy, PDFProgressData } from 'ng2-pdf-viewer';
   styleUrls: ['./pdf-renderer.component.scss']
 })
 export class PdfRendererComponent implements OnChanges {
+
   @Input() pdfSrc: string;
+  @Input() emptyStatusText: string = 'No se ha definido un archivo';
+  @Input() loadingStatusText: string = 'Cargando...';
+  @Input() loadedStatusText: string = 'Archivo cargado';
   @Output() loadedEvent: EventEmitter<boolean> = new EventEmitter();
+
   statusText: string;
   loadStatus: LoadStatus;
-  loadingPercentage:number;
-  constructor() { }
+  loadingPercentage: number;
   validatedSrc: string;
+
+  constructor() { }
+
   ngOnChanges(changes: SimpleChanges) {
     for (let propName in changes) {
       if (propName == 'pdfSrc') {
@@ -25,28 +32,26 @@ export class PdfRendererComponent implements OnChanges {
         if (curVal != prevVal && curVal != undefined) {
           this.loadPdf(curVal);
         }
-        else if(curVal==undefined)
-        {
+        else if (curVal == undefined) {
           this.loadedEvent.emit(false);
-          this.statusText='No se ha definido archivo';     
+          this.statusText = this.emptyStatusText;
+          this.loadStatus = LoadStatus.Empty;
         }
       }
     }
   }
-  loadPdf(pdfSrc:string) {
-    this.loadingPercentage=0;
-    this.validatedSrc=pdfSrc;
-   console.log(this.validatedSrc);
+  loadPdf(pdfSrc: string) {
+    this.loadingPercentage = 0;
+    this.validatedSrc = pdfSrc;
     this.loadedEvent.emit(false);
-    // mock: cargando
-    this.statusText = 'Cargando...';
+    this.statusText = this.loadingStatusText;
     this.loadStatus = LoadStatus.Loading;
   }
   pdfLoaded(pdf: PDFDocumentProxy) {
     this.loadedEvent.emit(true);
     this.loadStatus = LoadStatus.Loaded;
-    this.statusText = 'Cargado';
-    
+    this.statusText = this.loadedStatusText;
+
   }
   pdfLoadingError(error: any) {
     this.loadedEvent.emit(false);
@@ -55,7 +60,7 @@ export class PdfRendererComponent implements OnChanges {
     console.error(error);
   }
   onProgress(progressData: PDFProgressData) {
-    this.loadingPercentage=(progressData.loaded/progressData.total)*100;
+    this.loadingPercentage = (progressData.loaded / progressData.total) * 100;
   }
 
 }
